bb1fcd8bf2c40e55d6e740a07e46588bc0ff05dd
longfar <ningchangyuan@nuaa.edu.cn>
2024-06-21 02:41:29 +0000

    init: run pikiwidb test

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject jepsen-pikiwidb "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Jepsen test for PikiwiDB"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns jepsen-pikiwidb.core})
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [jepsen "0.1.18"]
+                 [com.taoensso/carmine "2.19.1"]]
+  :repl-options {:init-ns jepsen-pikiwidb.core}
+  :main jepsen-pikiwidb.core)
--- src/jepsen_pikiwidb/core.clj
+++ src/jepsen_pikiwidb/core.clj
@@ -1,6 +1,21 @@
-(ns jepsen-pikiwidb.core)
+(ns jepsen-pikiwidb.core
+  (:require [jepsen.cli :as cli]
+            [jepsen.tests :as tests]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn pikiwidb-test
+  "Builds up a PikiwiDB test"
+  [opts]
+  (merge tests/noop-test
+         {:pure-generators true}
+         opts))
+
+(defn -main
+  "Handles command line arguments. Can either run a test, or a web server for browing results"
+  [& args]
+  (cli/run! (cli/single-test-cmd {:test-fn pikiwidb-test})
+            args))
