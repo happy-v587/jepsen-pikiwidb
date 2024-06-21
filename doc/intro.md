2c5c9a70bdf6e301342fbc2c689050cac48f1bee
longfar <ningchangyuan@nuaa.edu.cn>
2024-06-21 01:22:38 +0000

    init: new lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jepsen-pikiwidb
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/jepsen_pikiwidb/core_test.clj
@@ -0,0 +1,7 @@
+(ns jepsen-pikiwidb.core-test
+  (:require [clojure.test :refer :all]
+            [jepsen-pikiwidb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
