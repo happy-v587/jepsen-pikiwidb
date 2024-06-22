b93c547b2c3548de438e8ddb0c9ee19a6263ed4c
longfar <email>
2024-06-22 06:25:42 +0000

    feat: build pikiwidb

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jepsen "0.1.18"]
+                 [jepsen "0.2.1-SNAPSHOT"]
                  [com.taoensso/carmine "2.19.1"]]
   :repl-options {:init-ns jepsen-pikiwidb.core}
   :main jepsen-pikiwidb.core)
--- src/jepsen_pikiwidb/core.clj
+++ src/jepsen_pikiwidb/core.clj
@@ -1,17 +1,73 @@
 (ns jepsen-pikiwidb.core
-  (:require [jepsen.cli :as cli]
-            [jepsen.tests :as tests]))
+  (:require [jepsen [db :as jdb]
+                    [cli :as cli]
+                    [tests :as tests]
+                    [control :as c]
+                    [util :as util] ]
+            [jepsen.os.ubuntu :as ubuntu]
+            [clojure.string :as str]
+            [clojure.java.shell :refer [sh]]
+            [slingshot.slingshot :refer [try+ throw+]]
+            [clojure.tools.logging :refer [info warn]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def build-file
+  "A file we create to track the last built version; speeds up compilation."
+  "jepsen-built-version")
+
+(def build-locks
+  "We use these locks to prevent concurrent builds."
+  (util/named-locks))
+
+(defmacro with-build-version
+  "Takes a test, a repo name, a version, and a body. Builds the repo by
+  evaluating body, only if it hasn't already been built. Takes out a lock on a
+  per-repo basis to prevent concurrent builds. Remembers what version was last
+  built by storing a file in the repo directory. Returns the result of body if
+  evaluated, or the build directory."
+  [node repo-name version & body]
+  `(util/with-named-lock build-locks [~node ~repo-name]
+     (let [build-file# (str "/" ~repo-name "/" build-file)]
+       (if (try+ (= (str ~version) (c/exec :cat build-file#))
+                (catch [:exit 1] e# ; Not found
+                  false))
+         ; Already built
+         (str "/" ~repo-name)
+         ; Build
+         (let [res# (do ~@body)]
+           ; Log version
+           (c/exec :echo ~version :> build-file#)
+           res#)))))
+
+(defn build-pikiwidb!
+  "Compiles pikiwidb, and returns the binary file we built."
+  [test node]
+  (let [version (:version test)]
+    (with-build-version node "pikiwidb" version
+      (info "Building pikiwidb" (:version test))
+      (c/cd "/pikiwidb"
+        (c/exec "./build.sh"))
+      "/pikiwidb/bin/pikiwidb")))
+
+(defn pdb 
+  "PikiwiDB"
+  [version]
+  (reify jdb/DB
+    (setup! [_ test node]
+      (info node "installing pikiwidb" (:version test))
+      (let [bin (build-pikiwidb! test node)]))
+
+    (teardown! [this test node]
+      (info node "tearing down pikiwidb" (:version test))
+      )))
 
 (defn pikiwidb-test
   "Builds up a PikiwiDB test"
   [opts]
   (merge tests/noop-test
-         {:pure-generators true}
+         {:db (pdb "0.0.1")
+          :version "0.0.1"
+          :os ubuntu/os
+          :pure-generators true}
          opts))
 
 (defn -main
